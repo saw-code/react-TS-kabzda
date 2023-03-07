0f5997bdb6c3870a54152ecf8aede6de592916d3
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-03-07 12:20:00 +0300

    useCallback hook

---
+++ src/components/UseCallback/UseCallback.tsx
@@ -0,0 +1,40 @@
+import {useCallback, useMemo, useState} from "react";
+
+export const LikeUseCallback = () => {
+  console.log("LikeUseCallback")
+  const [counter, setCounter] = useState(0)
+  const [books, setBooks] = useState(["React", "JS", "CSS", "HTML"])
+
+  const memoizedAddBook = useMemo(() => {
+    console.log(books)
+    return () => {
+      const newUsers = [...books, "Angular" + new Date().getTime()]
+      setBooks(newUsers)
+    }
+  }, [books])
+
+  const memoizedAddBook2 = useCallback(() => {
+    console.log(books)
+    const newUsers = [...books, "Angular" + new Date().getTime()]
+    setBooks(newUsers)
+  }, [books])
+
+  return <>
+    <button onClick={() => setCounter(counter + 1)}>+</button>
+    {counter}
+    {/*<Book addBook={memoizedAddBook}/>*/}
+  </>
+}
+
+type BookSecretPropsTpe = {
+  addBook: () => void
+}
+
+const BookSecret = (props: BookSecretPropsTpe) => {
+  console.log("BookSecret")
+  return <div>
+    <button onClick={() => props.addBook}>add book</button>
+  </div>
+}
+
+// const Book = React.memo(BookSecret)
